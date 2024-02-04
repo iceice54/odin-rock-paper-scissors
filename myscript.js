ac835a7346ff7d5847b1dfd594d657aa347598bb
iceice54 <125724815+iceice54@users.noreply.github.com>
2024-02-05 00:52:45 +0800

    Add getPlayerChoice function

--- myscript.js
+++ myscript.js
@@ -1,12 +1,30 @@
 function getComputerChoice() {
     //get random number from 0 to 2 and use 0, 1 and 2 to represent the 3 choices
     let randomNum = Math.floor(Math.random() * 3)
+
     switch(randomNum) {
         case 0:
             return "rock"
         case 1:
             return "paper"
         case 2:
-            return "stone"
+            return "scissors"
+    }
+}
+
+function getPlayerChoice() {
+    //set input value to lowercase to support uppercase and mixed case inputs
+    let playerChoice = prompt("Enter rock, paper or scissors.").toLowerCase()
+
+    //set default return to null so invalid inputs will return null
+    switch(playerChoice) {
+        case "rock":
+            return "rock"
+        case "paper":
+            return "paper"
+        case "scissors":
+            return "scissors"
+        default:
+            return null
     }
 }
